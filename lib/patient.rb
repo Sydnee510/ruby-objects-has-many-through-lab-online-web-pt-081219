fe13cdc7814764d9e31dc1661276d9d3b98c88b5
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 06:42:05 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,46 +1,22 @@
-# class Patient
-#   attr_accessor :name
-#   @@all = []
-#   def initialize(name)
-#     @name = name 
-#     @@all << self
-#   end
-#   def self.all 
-#     @@all
-#   end
-#   def appointments 
-#     Appointment.all.select {|appointment| appointment.patient == self}
-#   end
-#   def doctors 
-#     self.appointments.collect do |appointment|    
-#       appointment.doctor
-#     end
-#   end
-#   def new_appointment(date, doctor) 
-#     Appointment.new(self, date, doctor)
-#   end 
-# end 
 class Patient
-
-  attr_reader :name, :appointment, :doctor
-
+  attr_accessor :name
+  @@all = []
   def initialize(name)
-    @name = name
+    @name = name 
+    @@all << self
   end
-
-  def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
+  def self.all 
+    @@all
   end
-
-  def appointments
-    Appointment.all.select do |appointment|
-      appointment.patient == self
-    end
+  def appointments 
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
-
-  def doctors
-    Appointment.all.map do |appointment|
+  def doctors 
+    self.appointments.collect do |appointment|    
       appointment.doctor
     end
   end
-end
+  def new_appointment(date, doctor) 
+    Appointment.new(self, date, doctor)
+  end 
+end 
