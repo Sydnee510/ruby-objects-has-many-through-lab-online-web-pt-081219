d15a30854838170e6f42610bf25978913f3f90dc
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 05:04:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,13 +10,6 @@ class Artist
   end
   def new_song(name, genre) 
     Song.new(name, self, genre)
-    # song = Song.new(name)
-    # new_song(song)
-    # @songs << song
-    #song.artist = self
-    # Artist.new(@name)
-      #@songs << name
-      #name.artist = self
   end 
   def genres                      
     self.songs.collect do |song|    
@@ -24,7 +17,6 @@ class Artist
     end
   end
   def songs
-    #@songs
     Song.all.select {|song| song.artist == self}
   end
 end 
