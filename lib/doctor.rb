c3fa034576c4573329c3a43224559dc1c8e45b9b
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 05:28:17 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,8 +1,13 @@
 class Doctor 
   attr_accessor :name
+  @@all = []
   def initialize(name)
     @name = name 
+    @@all << self
   end 
+  def self.all 
+    @@all
+  end
   def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
