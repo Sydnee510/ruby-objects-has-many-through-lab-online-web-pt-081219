3886642ec14c590feed5e9882f2a8ef722c4b80d
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 06:45:09 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -20,3 +20,24 @@
 #     Appointment.new(self, date, doctor)
 #   end 
 # end 
+class Patient
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def doctors
+    self.appointments.collect {|appointment| appointment.doctor}
+  end
+end
