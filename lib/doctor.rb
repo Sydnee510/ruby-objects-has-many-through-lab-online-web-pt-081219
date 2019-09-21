1bbd7aa780f6d0acc5d50e292bb170ce481dc304
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 06:00:17 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
    def patients 
-     self.appointments.collect do |appointment|    
+     appointments.collect do |appointment|    
        appointment.patient
      end
  end 
