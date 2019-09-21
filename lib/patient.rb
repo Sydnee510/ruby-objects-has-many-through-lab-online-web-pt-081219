7c6b5a924c0ffefdb3b70b7e534fae9bc2ecbc12
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 06:46:12 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,43 +1,22 @@
-# class Patient
-#   attr_accessor :name
-#   @@all = []
-#   def initialize(name)
-#     @name = name 
-#     @@all << self
-#   end
-#   def self.all 
-#     @@all
-#   end
-#   def appointments 
-#     Appointment.all.select {|appointment| appointment.patient == self}
-#   end
-#   def doctors 
-#     self.appointments.collect do |appointment|    
-#       appointment.doctor
-#     end
-#   end
-#   def new_appointment(date, doctor) 
-#     Appointment.new(self, date, doctor)
-#   end 
-# end 
 class Patient
   attr_accessor :name
-
+  @@all = []
   def initialize(name)
-    @name = name
-    @appointments = []
+    @name = name 
+    @@all << self
   end
-
-  def new_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self
+  def self.all 
+    @@all
   end
-
-  def appointments
-    @appointments
+  def appointments 
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
-
-  def doctors
-    self.appointments.collect {|appointment| appointment.doctor}
+  def doctors 
+    self.appointments.collect do |appointment|    
+      appointment.doctor
+    end
   end
-end
+  def new_appointment(date, doctor) 
+    Appointment.new(self, date, doctor)
+  end 
+end 
