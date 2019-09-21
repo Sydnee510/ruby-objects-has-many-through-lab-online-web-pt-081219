a93726e47029bfb3f9019707b8b2f50f5e766888
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 06:14:43 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,18 +11,18 @@ class Patient
   def appointments 
     Appointment.all.select {|appointment| appointment.patient == self}
   end
-   def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
-  end
+  # def appointments
+  #   Appointment.all.select {|appointment| appointment.doctor == self}
+  # end
    def patients 
      appointments.collect do |appointment|    
        appointment.patient
      end
-  def doctors 
-    appointments.collect do |appointment|    
-      appointment.doctor
-    end
-  end
+  # def doctors 
+  #   appointments.collect do |appointment|    
+  #     appointment.doctor
+  #   end
+  # end
   def new_appointment(doctor, date) 
     Appointment.new(self,doctor, date)
   end 
