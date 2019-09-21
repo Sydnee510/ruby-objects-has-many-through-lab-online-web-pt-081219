0216fc2035710759dd48740cb414153597865617
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 07:20:25 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,8 @@
 class Patient
-  attr_accessor :name, :appointment, :doctor
+  attr_accessor :name, 
   @@all = []
   def initialize(name)
     @name = name 
-    @appointment = []
     @@all << self
   end
   def self.all 
